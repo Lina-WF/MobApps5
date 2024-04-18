beaa86dba0008ae727f747fad8e2beb4b6b79087
Ушакова Полина <ushakova.l1na21@gmail.com>
2024-04-18 22:29:13 +0300

    Ушакова Полина, №23, БСБО-09-21, пятая лабораторная

---
+++ settings.gradle.kts
@@ -0,0 +1,21 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "lesson5"
+include(":app")
+include(":listview")
+include(":accelerometer")
+include(":camera")
+include(":audiorecord")
